2518f6cb4300da9dae2a1315878eb36e7caa5de0
Diego Marques <fariia_di@hotmail.com>
2023-02-08 19:47:50 -0300

    Refatorando - Lista de Chamados nas classes Cliente e Tecnico

--- src/main/java/br/com/diegomarques/helpdesk/domain/Cliente.java
+++ src/main/java/br/com/diegomarques/helpdesk/domain/Cliente.java
@@ -1,9 +1,14 @@
 package br.com.diegomarques.helpdesk.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.diegomarques.helpdesk.domain.enums.Perfil;
 
 public class Cliente extends Pessoa{
 	private static final long serialVersionUID = 1L;
+	
+	private List<Chamado> chamados = new ArrayList<>(); //Iniciar o ArrayList para evitar ponteiro nulo
 
 	public Cliente() {
 		super();
@@ -13,6 +18,14 @@ public class Cliente extends Pessoa{
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
 	}
 
 	
--- src/main/java/br/com/diegomarques/helpdesk/domain/Tecnico.java
+++ src/main/java/br/com/diegomarques/helpdesk/domain/Tecnico.java
@@ -1,10 +1,16 @@
 package br.com.diegomarques.helpdesk.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.diegomarques.helpdesk.domain.enums.Perfil;
 
 public class Tecnico extends Pessoa{
 	private static final long serialVersionUID = 1L;
 
+	
+	private List<Chamado> chamados = new ArrayList<>();
+	
 	public Tecnico() {
 		super();
 		addPerfil(Perfil.CLIENTE);
@@ -13,6 +19,14 @@ public class Tecnico extends Pessoa{
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
 	}
 	
 	
