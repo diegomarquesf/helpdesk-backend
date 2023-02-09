4fe7c5d23e356bdfc4a3c64d888e5ce410282a57
Diego Marques <fariia_di@hotmail.com>
2023-02-09 10:53:07 -0300

    Excecao para objetos nao encontrados

--- src/main/java/br/com/diegomarques/helpdesk/services/TecnicoService.java
+++ src/main/java/br/com/diegomarques/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.diegomarques.helpdesk.domain.Tecnico;
 import br.com.diegomarques.helpdesk.repositories.TecnicoRepository;
+import br.com.diegomarques.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -17,7 +18,7 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> tecnico = tecnicoRepository.findById(id);
 		
-		return tecnico.orElse(null);
+		return tecnico.orElseThrow(() -> new ObjectNotFoundException("Objeto não Encontrado, ID: " +id));
 	}
 
 }
